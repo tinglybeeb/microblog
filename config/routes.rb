8509b3dc2f4c30d89ccf5b9098351ecd12a45f30
JWL <tinglybeeb@gmail.com>
2018-06-15 09:29:10 -0400

    set root route to point to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # going to the domain root URL will find the controller with prefix "Pages" and call its "home" action
   root to: 'pages#home'
   # for URL: root/pages/about, find the controller with prefix "Pages" and call its "about" action
   get 'pages/about', to: 'pages#about'
